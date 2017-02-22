561438614eebb423d9fa2a39b1ce26a47f33bb6d
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2017-02-22 23:48:12 +0330

    test added

--- test/tests/basic.js
+++ test/tests/basic.js
@@ -2,6 +2,7 @@ const test = require('./../common').test
 const expect = require('chai').expect
 const _send = test.sendMessage
 const TOTAL = require('./../common').TOTAL
+const http = require('http')
 
 let processes
 let identifiers = []
@@ -17,11 +18,26 @@ before(function (done) {
 })
 
 it('webpage loading', function (done) {
-  done()
+  http.get('http://127.0.0.1:7000', (res) => {
+    expect(res.headers['content-type']).to.equal('text/html; charset=UTF-8')
+    expect(res.statusCode).to.equal(200)
+    done()
+  })
 })
 
 it('api', function (done) {
-  done()
+  http.get('http://127.0.0.1:7000/all', (res) => {
+    expect(res.headers['content-type']).to.equal('application/json; charset=utf-8')
+    expect(res.statusCode).to.equal(200)
+
+    let raw = ''
+    res.on('data', (chunk) => raw += chunk)
+    res.on('end', () => {
+      let json = JSON.parse(raw)
+      expect(Object.keys(json).length).to.equal(3)
+      done()
+    })
+  })
 })
 
 after(function () {
